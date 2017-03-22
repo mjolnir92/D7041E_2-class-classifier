dc37595f301248fa114704cf769d8eff53e44138
Mikael Hedkvist <mikhed-1@student.ltu.se>
2017-03-22 23:36:53 +0100

    Classifier now working

--- NearestNeighbor.py
+++ NearestNeighbor.py
@@ -1,15 +1,59 @@
 import numpy as np
 
 class NearestNeighbor:
-	def train(self, values, labels):
-		pass
+	def train(self, values, labels, k):
+		self.values = values
+		self.labels = labels
+		self.k = k
 
 	def predict(self, values):
-		pass
+		test = values.shape[0]
+		predict = np.zeros(test, dtype = self.labels.dtype)
+		for i in xrange(test):
+			dist = np.sqrt(np.sum(np.square(self.values - values[i,:]), axis = 1))
+			predict[i] = self.kNN(dist, self.k)
+		return predict
 
 	def kNN(self, dist, k):
-		pass
+		near = np.argsort(dist)[:k]
+		ans = np.zeros(2)
+		for n in near:
+			ans[self.labels[n]] += 1
+		return np.argmax(ans)
 
-	def crossFoldValid(self, values, labels):
-		pass
+	def crossFoldValid(self, values, labels, k_max):
+		x = np.split(values, 3)
+		y = np.split(labels, 3)
+	
+		train_val = None
+		valid_val = None
+		train_lbl = None
+		valid_lbl = None
+
+		fold_results = []
+
+		for i in xrange(3):
+			print "fold ", i+1
+			train_val = np.concatenate((x[i%3], x[(i+1)%3]), axis = 0)
+			valid_val = x[(i+2)%3]
+			train_lbl = np.concatenate((y[i%3], y[(i+1)%3]), axis = 0)
+			valid_lbl = y[(i+2)%3]
+
+			accs = []
+
+			for j in range(1, k_max):
+				self.train(train_val, train_lbl, j)
+				pred = self.predict(valid_val)
+				acc = np.mean(pred == valid_lbl)
+				print "acc for k=", j, ": ", acc
+				accs.append(acc)
+
+			fold_results.append(accs)
+
+		totals = np.zeros(k_max)
+
+		for k in range(1, k_max):
+			totals[k] = fold_results[0][k-1] + fold_results[1][k-1] + fold_results[2][k-1]
+
+		return np.argmax(totals)
 
--- main.py
+++ main.py
@@ -1,22 +1,44 @@
 import numpy as np
 from NearestNeighbor import *
+from Utilities import *
 
-#read first 800 samples (first 2 persons) to a matrix.
+#read first 800 samples (first 2 persons) to a matrix
 raw_data = np.loadtxt(open("DSL-StrongPasswordData.csv", "rb"), dtype='string',  delimiter=",", skiprows=1)
 data = raw_data[:800,]
 
-#scramble rows, then split inte training and validation sets.
+#scramble rows, then split inte training and validation sets
 np.random.shuffle(data)
 train_data = data[:600,]
 valid_data = data[600:800,]
 
-#Format training labels and data for mathemagic
-train_labels = train_data[:,:1]
+#format training labels and data for mathemagic
+train_labels = train_data[:,:1].flatten()
+for i in xrange(train_labels.size):
+	if train_labels[i] == 's002':
+		train_labels[i] = int(0)
+	else:
+		train_labels[i] = int(1)
+train_labels = train_labels.astype(int)
 train_values = train_data[:,4:].astype(float)
 
-#Format validation labels and data for mathemagic
-valid_labels = valid_data[:,:1]
+#format validation labels and data for mathemagic
+valid_labels = valid_data[:,:1].flatten()
+for i in xrange(valid_labels.size):
+	if valid_labels[i] == 's002':
+		valid_labels[i] = int(0)
+	else:
+		valid_labels[i] = int(1)
+valid_labels = valid_labels.astype(int)
 valid_values = valid_data[:,4:].astype(float)
 
-print train_labels
-print train_values
+#instance of class
+nn = NearestNeighbor()
+
+#cross fold validate to find best k, then train nn with k, then predict labels
+k = nn.crossFoldValid(train_values, train_labels, 101)
+print "best k foud to be: ", k
+nn.train(train_values, train_labels, k)
+pred = nn.predict(valid_values)
+
+#print accuracy
+print 'accuracy: %f' % (np.mean(pred == valid_labels))
